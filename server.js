020ab0d1ccde37d8403918f82badc403c8cdb07a
liamsegura <liamdiegosegura@gmail.com>
2022-06-07 23:03:17 +0100

    back

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 const cors = require('cors')
-hljs = require('highlight.js');
-html = hljs.highlight('<h1>Hello World!</h1>', {language: 'xml'}).value
 
 
 app.use(cors())
