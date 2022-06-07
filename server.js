e600cfe2f404e7ed733484cceda58f22dfa3cd46
liamsegura <liamdiegosegura@gmail.com>
2022-06-07 21:45:58 +0100

    test server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const codeSnippets = [
         "bulletOne" : "Use Object.keys() to get the keys of the first object.",
         "bulletTwo" : "Use Object.prototype.hasOwnProperty() to check if the second object has a key that's in the first object.",
         "bulletThree" : "Use Array.prototype.filter() to filter out keys that aren't in both objects.",
-        "code" : "const commonKeys = (obj1, obj2) => Object.keys(obj1).filter(key => obj2.hasOwnProperty(key));",
+        "code" : "const commonKeys = (obj1, obj2) => <br> Object.keys(obj1).filter(key => obj2.hasOwnProperty(key));",
         "execution" : "commonKeys({ a: 1, b: 2 }, { a: 2, c: 1 }); // ['a']"
     },
         {"name" : "JSON to file",
