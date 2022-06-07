e26e08b246c23eba643023df2f65c7fd93aef78f
liamsegura <liamdiegosegura@gmail.com>
2022-06-07 21:43:20 +0100

    randomised

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 
 const codeBlock = document.querySelector('code')
+const randomNumber = Math.random()
 
 async function apiRequest(){
     // const rapperName = document.querySelector('input').value
@@ -7,7 +8,9 @@ async function apiRequest(){
         const response = await fetch(`https://code-snippets-node-api.herokuapp.com/api`)
         const data = await response.json()
         console.log(data[0].code)
-        codeBlock.innerText = data[0].code
+        let code = ""
+        if(randomNumber < .5 ? code = data[1].code : code = data[0].code)
+        codeBlock.innerText = code
      
     }catch(error){
         console.log(error)
