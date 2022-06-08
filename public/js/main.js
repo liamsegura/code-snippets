73a4277029875f013c7ce3951f5782d66d831f5d
liamsegura <liamdiegosegura@gmail.com>
2022-06-08 18:38:02 +0100

    added more info btn

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,15 @@
 const snippetType = document.getElementById('language-copy')
 const snippetName = document.getElementById('name')
-
+const snippetDescription = document.getElementById('description')
+const snippetBulletOne = document.getElementById('bullet-one')
+const snippetBulletTwo = document.getElementById('bullet-two')
+const snippetBulletThree = document.getElementById('bullet-three')
+const snippetBulletFour = document.getElementById('bullet-four')
+const snippetBulletFive = document.getElementById('bullet-five')
 const codeBlock = document.querySelector('code')
 const randomNumber = Math.random()
+
+
 
 document.querySelector('button').addEventListener('click', apiRequest)
 
@@ -11,11 +18,19 @@ async function apiRequest(){
     try{
         const response = await fetch(`https://code-snippets-node-api.herokuapp.com/api`)
         const data = await response.json()
-        console.log(data[0].code)
 
-        snippetType.innerText = data[1].tag
-        snippetName.innerText = data[1].name
-        codeBlock.innerText = data[1].code
+    //randomly choose object from object array
+          let randomObject = data[Math.floor(Math.random()*data.length)];
+    //place random object into DOM
+        snippetType.innerText = randomObject.tag
+        snippetName.innerText = randomObject.name
+        codeBlock.innerText = randomObject.code
+        snippetDescription.innerText = randomObject.description
+        snippetBulletOne.innerText = randomObject.bulletOne
+        snippetBulletTwo.innerText = randomObject.bulletTwo
+        snippetBulletThree.innerText = randomObject.bulletThree
+        snippetBulletFour.innerText = randomObject.bulletFour
+        snippetBulletFive.innerText = randomObject.bulletFive
 
         console.log(data[0].type)
      
@@ -63,3 +78,10 @@ async function apiRequest(){
     };
   
     copyButton.addEventListener('click', copyTextHandler);
+
+// more info btn
+
+document.querySelector('.more-info-btn').addEventListener('click', () => {
+
+  document.querySelector('.more-info-container').classList.toggle('show')
+})
